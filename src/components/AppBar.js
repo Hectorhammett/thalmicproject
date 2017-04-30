13654b16652f97cd04861786d179e19973e903ad
Hector Mendoza <Hector Mendoza>
2017-04-30 06:50:27 -0700

    Added a badge for responsive app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Logo from '../images/thlabs.svg';
+import Badge from '../images/th-badge.png'
 
 class AppBar extends Component {
     render() {
         return (
             <div className="app-bar">
-                <img src={Logo} className="logo" alt="Thalmic Labs Logo"/>
+                <img src={Logo} className="logo hidden-xs-inline" alt="Thalmic Labs Logo"/>
+                <img src={Badge} className="logo visible-xs-inline" alt="Thalmic Labs Logo"/>
                 <h1>Take Home Challenge</h1>
             </div>
         );
