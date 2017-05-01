e457f1f02dcf7ec025ce1df8e7c242c5338590f2
Hector Mendoza <Hector Mendoza>
2017-05-01 03:41:40 -0700

    Added the opn package

--- web-server.js
+++ web-server.js
@@ -1,14 +1,16 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+let opn = require('opn');
+let port = 8080;
 
 app.use(express.static(path.join(__dirname + '/dist/javascript')));
 
 app.get('/', function (req, res) {
-    console.log(path.join(__dirname + '/dist/index.html'));
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(port, function () {
+    console.log(`App listening to port ${port}`);
+    opn(`http://localhost:${port}`);
+})  
